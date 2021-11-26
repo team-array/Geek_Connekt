c0fcc5f4102a4f9abb56afb3e03202ec145420f5
Kranthi Kumar <rareone.kk@gmail.com>
2021-11-26 15:51:48 +0530

    added notification

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,8 @@ import "antd/dist/antd.css";
 import Home from "./pages/Home/Home";
 import Signin from "./pages/Signin/Signin";
 import Dashboard from "./pages/Geeks-Connect/DashBoard/Dashboard";
+import Notifications from "./pages/Geeks-Connect/Notifications/Notifications";
+import Profile from "./pages/Geeks-Connect/Profile/Profile";
 import AOS from "aos";
 
 
@@ -23,6 +25,8 @@ const App = () => {
             <Route path="/" element={<Home />} />
             <Route path="/login" element={<Signin />} />
             <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/notification" element={<Notifications />} />
+            <Route path="/profile" element={<Profile />} />
           </Routes>
         </HashRouter>
       </div>
--- frontend/src/pages/Geeks-Connect/Notifications/Notifications.jsx
+++ frontend/src/pages/Geeks-Connect/Notifications/Notifications.jsx
@@ -1,11 +1,100 @@
 import React from 'react'
-
-const Notifications = () => {
+import './Notifications.scss'
+export default function Notifications() {
     return (
         <div>
-            
+            <div class="notifications">
+        <div class="icon_wrap"><i class="far fa-bell"></i></div>
+        
+        <div class="notification_dd">
+            <ul class="notification_ul">
+                <li class="starbucks success">
+                    <div class="notify_icon">
+                        <span class="icon"></span>  
+                    </div>
+                    <div class="notify_data">
+                        <div class="title">
+                            Lorem, ipsum dolor.  
+                        </div>
+                        <div class="sub_title">
+                          Lorem ipsum dolor sit amet consectetur.
+                      </div>
+                    </div>
+                    <div class="notify_status">
+                        <p>Like</p>  
+                    </div>
+                </li>  
+                <li class="baskin_robbins failed">
+                    <div class="notify_icon">
+                        <span class="icon"></span>  
+                    </div>
+                    <div class="notify_data">
+                        <div class="title">
+                            Lorem, ipsum dolor.  
+                        </div>
+                        <div class="sub_title">
+                          Lorem ipsum dolor sit amet consectetur.
+                      </div>
+                    </div>
+                    <div class="notify_status">
+                        <p>News</p>  
+                    </div>
+                </li> 
+                <li class="mcd success">
+                    <div class="notify_icon">
+                        <span class="icon"></span>  
+                    </div>
+                    <div class="notify_data">
+                        <div class="title">
+                            Lorem, ipsum dolor.  
+                        </div>
+                        <div class="sub_title">
+                          Lorem ipsum dolor sit amet consectetur.
+                      </div>
+                    </div>
+                    <div class="notify_status">
+                        <p>Like</p>  
+                    </div>
+                </li>  
+                <li class="pizzahut failed">
+                    <div class="notify_icon">
+                        <span class="icon"></span>  
+                    </div>
+                    <div class="notify_data">
+                        <div class="title">
+                            Lorem, ipsum dolor.  
+                        </div>
+                        <div class="sub_title">
+                          Lorem ipsum dolor sit amet consectetur.
+                      </div>
+                    </div>
+                    <div class="notify_status">
+                        <p>News</p>  
+                    </div>
+                </li> 
+                <li class="kfc success">
+                    <div class="notify_icon">
+                        <span class="icon"></span>  
+                    </div>
+                    <div class="notify_data">
+                        <div class="title">
+                            Lorem, ipsum dolor.  
+                        </div>
+                        <div class="sub_title">
+                          Lorem ipsum dolor sit amet consectetur.
+                      </div>
+                    </div>
+                    <div class="notify_status">
+                        <p>Like</p>  
+                    </div>
+                </li> 
+                <li class="show_all">
+                    <p class="link">Show All Activities</p>
+                </li> 
+            </ul>
+        </div>
+        
+      </div>
         </div>
     )
 }
-
-export default Notifications
