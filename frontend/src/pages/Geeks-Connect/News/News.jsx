7820694dcdb9c1c3127b4be5dd4c5fafee18e7f5
Kranthi Kumar <rareone.kk@gmail.com>
2021-12-01 20:29:24 +0530

    added new

--- frontend/src/pages/Geeks-Connect/News/News.jsx
+++ frontend/src/pages/Geeks-Connect/News/News.jsx
@@ -1,9 +1,153 @@
 import React from 'react'
 
+import "antd/dist/antd.css";
+import { Card } from 'antd';
+const { Meta } = Card
+
+
 const News = () => {
     return (
         <div>
+        <div id="feature" className="block featureBlock bgGray" style={{padding:'0 100px'}}>
+        <div className="container-fluid">
+          <div className="titleHolder">
+            <h2 style={{marginBottom:'25px'}}>News Corner</h2>
+            <p></p>
+          </div>
+          <Row gutter={[16, 16]}>
+            <Col xs={{ span: 24 }} sm={{ span: 12 }} md={{ span: 8 }}>
+              <Card
+                hoverable
+                
+              >
+                <Meta title="News" />
+                <hr />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo fugiat sint tempora cupiditate, voluptates quo earum temporibus suscipit minima dolor iste. Hic reiciendis consectetur assumenda necessitatibus atque doloremque adipisci obcaecati?</p>
+              </Card>
+            </Col>
+            <Col xs={{ span: 24 }} sm={{ span: 12 }} md={{ span: 8 }}>
+              <Card
+                hoverable
+                
+              >
+                <Meta title="News" />
+                <hr />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo fugiat sint tempora cupiditate, voluptates quo earum temporibus suscipit minima dolor iste. Hic reiciendis consectetur assumenda necessitatibus atque doloremque adipisci obcaecati?</p>
+              </Card>
+            </Col>
+            <Col xs={{ span: 24 }} sm={{ span: 12 }} md={{ span: 8 }}>
+              <Card
+                hoverable
+                
+              >
+                <Meta title="News" />
+                <hr />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo fugiat sint tempora cupiditate, voluptates quo earum temporibus suscipit minima dolor iste. Hic reiciendis consectetur assumenda necessitatibus atque doloremque adipisci obcaecati?</p>
+              </Card>
+            </Col>
+            <Col xs={{ span: 24 }} sm={{ span: 12 }} md={{ span: 8 }}>
+              <Card
+                hoverable
+                
+              >
+                <Meta title="News" />
+                <hr />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo fugiat sint tempora cupiditate, voluptates quo earum temporibus suscipit minima dolor iste. Hic reiciendis consectetur assumenda necessitatibus atque doloremque adipisci obcaecati?</p>
+              </Card>
+            </Col>
+            <Col xs={{ span: 24 }} sm={{ span: 12 }} md={{ span: 8 }}>
+              <Card
+                hoverable
+                
+              >
+                <Meta title="News" />
+                <hr />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo fugiat sint tempora cupiditate, voluptates quo earum temporibus suscipit minima dolor iste. Hic reiciendis consectetur assumenda necessitatibus atque doloremque adipisci obcaecati?</p>
+              </Card>
+            </Col>
+            <Col xs={{ span: 24 }} sm={{ span: 12 }} md={{ span: 8 }}>
+              <Card
+                hoverable
+                
+              >
+                <Meta title="News" />
+                <hr />
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo fugiat sint tempora cupiditate, voluptates quo earum temporibus suscipit minima dolor iste. Hic reiciendis consectetur assumenda necessitatibus atque doloremque adipisci obcaecati?</p>
+              </Card>
+            </Col>
+            <Col xs={{ span: 24 }} sm={{ span: 12 }} md={{ span: 8 }}>
+            <Card
+              hoverable
+              
+            >
+              <Meta title="News" />
+              <hr />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo fugiat sint tempora cupiditate, voluptates quo earum temporibus suscipit minima dolor iste. Hic reiciendis consectetur assumenda necessitatibus atque doloremque adipisci obcaecati?</p>
+            </Card>
+          </Col>
+          <Col xs={{ span: 24 }} sm={{ span: 12 }} md={{ span: 8 }}>
+          <Card
+            hoverable
             
+          >
+            <Meta title="News" />
+            <hr />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo fugiat sint tempora cupiditate, voluptates quo earum temporibus suscipit minima dolor iste. Hic reiciendis consectetur assumenda necessitatibus atque doloremque adipisci obcaecati?</p>
+          </Card>
+        </Col>
+        <Col xs={{ span: 24 }} sm={{ span: 12 }} md={{ span: 8 }}>
+        <Card
+          hoverable
+          
+        >
+          <Meta title="News" />
+          <hr />
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo fugiat sint tempora cupiditate, voluptates quo earum temporibus suscipit minima dolor iste. Hic reiciendis consectetur assumenda necessitatibus atque doloremque adipisci obcaecati?</p>
+        </Card>
+      </Col>
+      <Col xs={{ span: 24 }} sm={{ span: 12 }} md={{ span: 8 }}>
+      <Card
+        hoverable
+        
+      >
+        <Meta title="News" />
+        <hr />
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo fugiat sint tempora cupiditate, voluptates quo earum temporibus suscipit minima dolor iste. Hic reiciendis consectetur assumenda necessitatibus atque doloremque adipisci obcaecati?</p>
+      </Card>
+    </Col>
+    <Col xs={{ span: 24 }} sm={{ span: 12 }} md={{ span: 8 }}>
+    <Card
+      hoverable
+      
+    >
+      <Meta title="News" />
+      <hr />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo fugiat sint tempora cupiditate, voluptates quo earum temporibus suscipit minima dolor iste. Hic reiciendis consectetur assumenda necessitatibus atque doloremque adipisci obcaecati?</p>
+    </Card>
+  </Col>
+  <Col xs={{ span: 24 }} sm={{ span: 12 }} md={{ span: 8 }}>
+  <Card
+    hoverable
+    
+  >
+    <Meta title="News" />
+    <hr />
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo fugiat sint tempora cupiditate, voluptates quo earum temporibus suscipit minima dolor iste. Hic reiciendis consectetur assumenda necessitatibus atque doloremque adipisci obcaecati?</p>
+  </Card>
+  </Col>
+  <Col xs={{ span: 24 }} sm={{ span: 12 }} md={{ span: 8 }}>
+  <Card
+    hoverable
+    
+  >
+    <Meta title="News" />
+    <hr />
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo fugiat sint tempora cupiditate, voluptates quo earum temporibus suscipit minima dolor iste. Hic reiciendis consectetur assumenda necessitatibus atque doloremque adipisci obcaecati?</p>
+  </Card>
+  </Col>
+          </Row>
+        </div>
+      </div>
         </div>
     )
 }
