3041ae623ce79114bb46eb600cbaa459ffea8fbe
DeepeshDragoneel <deepeshash444@gmail.com>
2021-12-06 22:36:30 +0530

    Added basic attendance

--- backend/controllers/AttendanceController.js
+++ backend/controllers/AttendanceController.js
@@ -1,25 +1,49 @@
 const xlsx = require("xlsx");
+const Attendance = require("../models/attendance");
+const User = require("../models/user");
 
-exports.uploadAttendance = (req, res) => {
+exports.uploadAttendance = async (req, res) => {
     console.log("uploadAttendance");
-    console.log(req.body);
-    console.log(req.files);
+    // console.log(req.body);
+    // console.log(req.files);
     let file = req.files.file;
     var workbook = xlsx.readFile(file.tempFilePath);
     var sheet_name_list = workbook.SheetNames;
     var xlData = xlsx.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
     console.log(xlData);
-    // const workBook = xlsx.readFile(file.tempFilePath);
-    // const worksheet = workBook.Sheets[workBook.SheetNames[0]];
-    // console.log(workBook);
-    // let posts = [];
-    // let post = {};
-
-    // for (let cell in worksheet) {
-    //     const cellAsString = cell.toString();
-
-    //     const row = cellAsString.match(/\d+/g);
-    // }
-
-    // console.log(posts);
+    try {
+        xlData.forEach(async (element, idx) => {
+            if (idx > 0) {
+                const user = await User.findOne({
+                    rollNumber: element.RollNumber,
+                });
+                if (user) {
+                    let attendanceList = [];
+                    for (var key of Object.keys(element)) {
+                        if (key !== "RollNumber") {
+                            console.log(
+                                `${key} : ${element[key]} Total: ${xlData[0][key]}`
+                            );
+                            const temp = {
+                                attendanceCategory: {
+                                    className: key,
+                                    totalClasses: xlData[0][key],
+                                    attendedClasses: element[key],
+                                },
+                            };
+                            attendanceList.push(temp);
+                        }
+                    }
+                    const attendance = new Attendance({
+                        userId: user._id,
+                        attendanceList: attendanceList,
+                    });
+                    console.log(attendance);
+                    attendance.save();
+                }
+            }
+        });
+    } catch (err) {
+        console.log(err);
+    }
 };
