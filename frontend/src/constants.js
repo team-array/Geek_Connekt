0da6e98cdeaf95fa8915cf4663606190af54d439
akshaymurari <akshaymurari184@gmail.com>
2021-12-11 15:40:08 +0530

    chats

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,6 +1,6 @@
 export const AppName = "Geeks Connect";
 
-export const BaseUrl = "http://localhost:8000";
+export const BaseUrl = "http://localhost:80";
 
 export const ClientUrl = "http://localhost:3000";
 
